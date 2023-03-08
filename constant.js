39a33bc466b30e482dd1e558534428194ca2a55b
toshitehdev <toshitehdev@proton.me>
2023-03-08 13:55:06 +0800

    add logging events

---
+++ constant.js
@@ -0,0 +1,745 @@
+const contractABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_genesis_address",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "_base_uri",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "ClaimBounty",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "minter",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "prize_amount",
+        type: "uint256",
+      },
+    ],
+    name: "EligibleBounty",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        components: [
+          {
+            internalType: "bool",
+            name: "is_eligible",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "prize_amount",
+            type: "uint256",
+          },
+          {
+            internalType: "bool",
+            name: "is_claimed",
+            type: "bool",
+          },
+          {
+            internalType: "bool",
+            name: "from_claiming",
+            type: "bool",
+          },
+        ],
+        indexed: false,
+        internalType: "struct ErcOrdinal.EligiblePrize",
+        name: "eligible_prize",
+        type: "tuple",
+      },
+    ],
+    name: "EligibleIds",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "_to",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "Mint",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+    ],
+    name: "UnclaimedBounty",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_spender",
+        type: "address",
+      },
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "base_uri",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "claimBounty",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+    ],
+    name: "claimViaErc721",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "ercordinal_erc721",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "genesis_address",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+    ],
+    name: "getAddressToIds",
+    outputs: [
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getGenesisSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_token_id",
+        type: "uint256",
+      },
+    ],
+    name: "getIdToIndex",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "index",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct ErcOrdinal.TokenIndex",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "getIdToTokens",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "idIsEligible",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "is_eligible",
+        type: "bool",
+      },
+      {
+        internalType: "uint256",
+        name: "prize_amount",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "is_claimed",
+        type: "bool",
+      },
+      {
+        internalType: "bool",
+        name: "from_claiming",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "idToEligibleForBounty",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "is_eligible",
+        type: "bool",
+      },
+      {
+        internalType: "uint256",
+        name: "prize_amount",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "idToTokens",
+    outputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "mintMany",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "mint_price",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "price_addition",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256[]",
+        name: "_eligible_ids",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "setEligibleIds",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_erc721_address",
+        type: "address",
+      },
+    ],
+    name: "setErc721Address",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256[]",
+        name: "_eligible_ids",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "setUniswapResurrection",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "the_creator",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "token_counter",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_sender",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256[]",
+        name: "_ids",
+        type: "uint256[]",
+      },
+    ],
+    name: "transferMany",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "transferSingle",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdrawMintSale",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+module.exports = { contractABI };
--- server.js
+++ server.js
@@ -1,8 +1,148 @@
 const express = require("express");
+const ethers = require("ethers");
+const cors = require("cors");
+const { contractABI } = require("./constant");
+const { db } = require("./config");
+require("custom-env").env(true);
+
+const contract_address = process.env.CONTRACT_ADDRESS;
+const provider = new ethers.getDefaultProvider(process.env.PROVIDER);
+const contract = new ethers.Contract(contract_address, contractABI, provider);
 
 const app = express();
 
 const port = 5000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const uu = async () => {
+  contract.on("EligibleIds", (id, eligible_prize) => {
+    const obj = {
+      id: ethers.toNumber(id),
+      is_eligible: eligible_prize.is_eligible,
+      prize_amount: ethers.toNumber(eligible_prize.prize_amount),
+      is_claimed: eligible_prize.is_claimed,
+      from_claiming: eligible_prize.from_claiming,
+    };
+    console.log(obj);
+    //get all eligible ids
+    //uniswap resurrect use the same event
+    db.collection("EligibleIds")
+      .doc(String(ethers.toNumber(id)))
+      .set(obj)
+      .then(() => {
+        console.log("data created");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  contract.on("UnclaimedBounty", (id) => {
+    //update existing id to from_claiming = true
+    db.collection("EligibleIds")
+      .doc(String(ethers.toNumber(id)))
+      .update({ from_claiming: true })
+      .then(() => {
+        console.log("updated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  contract.on("ClaimBounty", (id, amount) => {
+    //update existing id to is_claimed = true
+    db.collection("EligibleIds")
+      .doc(String(ethers.toNumber(id)))
+      .update({ is_claimed: true })
+      .then(() => {
+        console.log("updated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+uu();
+
+//get all eligibleids
+app.get("/logs", async (req, res) => {
+  try {
+    const idRef = db.collection("EligibleIds");
+    const response = await idRef.get();
+    const respArr = [];
+    response.forEach((doc) => {
+      respArr.push(doc.data());
+    });
+    res.send(respArr);
+  } catch (error) {
+    res.send(error);
+  }
+});
+//get all the logs
+app.get("/logs", async (req, res) => {
+  try {
+    const idRef = db.collection("EligibleIds");
+    const response = await idRef.get();
+    const respArr = [];
+    response.forEach((doc) => {
+      respArr.push(doc.data());
+    });
+    res.send(respArr);
+  } catch (error) {
+    res.send(error);
+  }
+});
+//get only the unclaimed id
+app.get("/logs/unclaimed", async (req, res) => {
+  try {
+    const idRef = db.collection("EligibleIds");
+    const response = await idRef.get();
+    const respArr = [];
+    response.forEach((doc) => {
+      if (!doc.data().is_claimed && !doc.data().from_claiming)
+        respArr.push(doc.data());
+    });
+    res.send(respArr);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+//get only the claimed id
+app.get("/logs/claimed", async (req, res) => {
+  try {
+    const idRef = db.collection("EligibleIds");
+    const response = await idRef.get();
+    const respArr = [];
+    response.forEach((doc) => {
+      if (doc.data().is_claimed) respArr.push(doc.data());
+    });
+    res.send(respArr);
+  } catch (error) {
+    res.send(error);
+  }
+});
+//get only the the expired ids
+app.get("/logs/expired", async (req, res) => {
+  try {
+    const idRef = db.collection("EligibleIds");
+    const response = await idRef.get();
+    const respArr = [];
+    response.forEach((doc) => {
+      if (doc.data().from_claiming) respArr.push(doc.data());
+    });
+    res.send(respArr);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 app.listen(port, () => {
   console.log("listening on:", port);
